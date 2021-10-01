b9038779fb6fba5bc707e3ed8d8f7b3e352bcb8c
DasPeTe <daspetemail@gmail.com>
2021-10-01 19:32:35 +0200

    feat: optimized region selection of recorded microphone sample

--- components/Waveform.vue
+++ components/Waveform.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="waveform w-full">
-        <div class="w-full">
+        <!-- <div class="w-full">
             <input class="w-full" type="range" min="0" max="100" step="0.01" v-model="StartRange">
-        </div>
+        </div> -->
         <div class="waveform__content w-full" ref="waveform"></div>
-        <div class="w-full">
+        <!-- <div class="w-full">
             <input class="w-full" type="range" min="0" max="100" step="0.01" v-model="EndRange">
-        </div>
+        </div> -->
     </div>
 </template>
 
 <script>
 let WaveSurfer = null
-if(!process.server) WaveSurfer = require('wavesurfer.js')
+let WaveSurferRegionsPlugin = null
+if(!process.server) {
+    WaveSurfer = require('wavesurfer.js')
+    WaveSurferRegionsPlugin = require('wavesurfer.js/dist/plugin/wavesurfer.regions.min.js')
+}
 
 export default {
     props: {
@@ -67,11 +71,31 @@ export default {
             if(this.wavesurfer) this.wavesurfer.destroy()
             this.wavesurfer = WaveSurfer.create({
                 container: this.$refs.waveform,
+                plugins: [
+                    WaveSurferRegionsPlugin.create({})
+                ],
                 ...this.options
             })
             this.wavesurfer.loadBlob(this.track.recordedSampleData)
             this.wavesurfer.on('ready', () => {
                 this.duration = this.wavesurfer.getDuration()
+
+                let startTime = this.track.sampleStartTime
+                let endTime = this.track.sampleStartTime + (this.track.sampleDuration > 0 ? this.track.sampleDuration : this.duration) - 0.0001
+
+                this.wavesurfer.addRegion({
+                    start: startTime,
+                    end: endTime,
+                    minLength: 0.01,
+                    maxLength: this.duration,
+                    drag: false,
+                    color: 'rgba(255, 150, 0, 0.15)'
+                })
+
+                this.wavesurfer.on('region-updated', (region) => {
+                    this.track.sampleStartTime = region.start
+                    this.track.sampleDuration = region.end - region.start
+                })
             })
             
         }
